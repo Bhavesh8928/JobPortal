1c5817defea5e5348db8f4714aee3b02617458d3
Bhavesh8928 <118186032+Bhavesh8928@users.noreply.github.com>
2023-04-11 01:08:14 +0530

    Delete src/main/java/com/bs directory

--- src/main/java/com/bs/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bs.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.bs.modal.Student;
-
-public interface StudentRepository extends CrudRepository<Student, Integer> 
-{	
-	
-}
